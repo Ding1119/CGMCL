f75eb887414123c3d97f22f1e5ee60b4061a7fc4
test <test@testdeMacBook-Air.local>
2023-07-05 00:40:33 +0800

    add args

---
+++ run_exp_skin.sh
@@ -0,0 +1,5 @@
+python3 run_gat_gat_skin.py --skin_type "dermatology_images/" \
+--meta_data_dir "/Users/test/Documents/Contrastive_PD/skin_dataset_ok/meta_ok/" \
+--losses_choice "Contrastive_loss" \
+--classes "PIG" \
+--n_epoch 3 >> logs.txt
--- run_gat_gat_skin.py
+++ run_gat_gat_skin.py
@@ -23,191 +23,195 @@ from sklearn import preprocessing
 from utils import build_knn_graph, label_return, plot_ROC, calculate_metrics_new
 from model import Projection, Model, CNN
 from losses import WeightedCrossEntropyLoss, contrastive_loss, info_loss, MGECLoss, SACLoss
+import argparse
 # device = torch.device("mps" if torch.cuda.is_available() else "cpu")
-device = torch.device("mps")
+
+def train_eval(datadir,skin_type, metadir, loss_select ,classes, epoch):
+    device = torch.device("mps")
+
+    resnet = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+    resnet = resnet.to(device)
+    # 将最后一层的输出维度修改为类别数目
+    num_classes = 1024
+    num_features = resnet.fc.in_features
+    resnet.fc = nn.Linear(num_features, num_classes)
+    resnet.fc = resnet.fc.to(device)
+
+    path_img = datadir + skin_type
+    path_meta = metadir
+    raw_image_train = np.load(path_img  + 'train_derm_img_413.npy') 
+
+    raw_image_test = np.load(path_img  + 'test_derm_img_395.npy') 
+
+    # raw_image_train = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/clinical_images/train_clinic_f_413.npy') /255
+    # raw_image_test = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/clinical_images/test_clinic_f_395.npy') /255
 
 
+    raw_f_train = np.load(path_meta + 'meta_train_413.npy')
+    raw_f_test = np.load(path_meta +  'meta_test_395.npy')
+    raw_f_train = preprocessing.scale(raw_f_train )
+    raw_f_test = preprocessing.scale(raw_f_test )
 
-resnet = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
-resnet = resnet.to(device)
-# 将最后一层的输出维度修改为类别数目
-num_classes = 1024
-num_features = resnet.fc.in_features
-resnet.fc = nn.Linear(num_features, num_classes)
-resnet.fc = resnet.fc.to(device)
+    image_data_train = raw_image_train 
+    feature_data_train = raw_f_train 
 
-raw_image_train = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/dermatology_images/train_derm_img_413.npy') 
-
-raw_image_test = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/dermatology_images/test_derm_img_395.npy') 
-
-# raw_image_train = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/clinical_images/train_clinic_f_413.npy') /255
-# raw_image_test = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/clinical_images/test_clinic_f_395.npy') /255
-
-
-raw_f_train = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/meta_ok/meta_train_413.npy')
-raw_f_test = np.load('/Users/test/Documents/Contrastive_PD/skin_dataset_ok/meta_ok/meta_test_395.npy')
-raw_f_train = preprocessing.scale(raw_f_train )
-raw_f_test = preprocessing.scale(raw_f_test )
-
-image_data_train = raw_image_train 
-feature_data_train = raw_f_train 
-
-# 转换为PyTorch张量
-image_data_train = torch.from_numpy(image_data_train ).float()
-image_data_train = torch.tensor(image_data_train ).transpose(1,3)
-feature_data_train = torch.from_numpy(feature_data_train).float()
-# import pdb;pdb.set_trace()
-image_data_flatten = torch.flatten(image_data_train, start_dim=1)
-image_data_flatten = image_data_flatten 
-
-# adj_train_img = kneighbors_graph(np.array(image_data_flatten), 200, mode='connectivity', include_self=True).toarray()
-
-adj_train_img = build_knn_graph(image_data_flatten,300).float()
-# import pdb;pdb.set_trace()
-# adj_train_img = torch.from_numpy(adj_train_img).float()
-
-
-image_data_test = torch.from_numpy(raw_image_test ).float()
-image_data_test = torch.tensor(image_data_test ).transpose(1,3)
-data_features_test = raw_f_test 
-test_feature_data = torch.from_numpy(data_features_test).float()
-
-# 创建测试用的邻接矩阵（这里假设所有病人之间都有连接）
-# test_adjacency_matrix = torch.ones((100, 100))
-
-
-## testing image adj
-
-image_data_test_flatten = torch.flatten(image_data_test, start_dim=1)
-# image_data_test_flatten = image_data_test
-# adj_test_img = kneighbors_graph(np.array(image_data_test_flatten), 200, mode='connectivity', include_self=True).toarray()
-
-adj_test_img = build_knn_graph(image_data_test_flatten, 200).float()
-
-
-adj_f_knn_train =  build_knn_graph(raw_f_train, 300).float()
-# adj_f_knn_train = adj_f_knn_train.toarray()
-# adj_f_knn_train = torch.from_numpy(adj_f_knn_train).float()
-# adj_f_knn_test = kneighbors_graph(np.array(raw_f_test), 300, mode='connectivity', include_self=True)
-adj_f_knn_test = build_knn_graph(raw_f_test, 300).float()
-
-projection = Projection(262, 3)
-model = Model(projection, resnet).to(device)
-cnn_encoder = CNN().to(device)
-
-class_weights = torch.tensor([0.5, 0.5,0.5])  # 自定義的類別權重
-criterion5 = WeightedCrossEntropyLoss(weight=class_weights)
-criterion2 = contrastive_loss
-# criterion3 = SACLoss()
-criterion3 = MGECLoss()
-# criterion3 = info_loss
-
-# criterion3 = loss_dependence
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-
-
-
-class_name = "PIG"
-
-n_epochs = 300
-training_range = tqdm(range(n_epochs))
-
-for epoch in training_range:
-    optimizer.zero_grad()
-    # cnn_z  =  cnn_encoder(image_data)
-    # 前向传播
-    
-    image_data_train = image_data_train.to(device)
-    feature_data_train = feature_data_train.to(device)
-    adj_train_img = adj_train_img.to(device)
-    adj_f_knn_train = adj_f_knn_train.to(device)
-    
-    
-    output1, output2, emb = model(image_data_train , feature_data_train,adj_train_img, adj_f_knn_train)
-
+    # 转换为PyTorch张量
+    image_data_train = torch.from_numpy(image_data_train ).float()
+    image_data_train = torch.tensor(image_data_train ).transpose(1,3)
+    feature_data_train = torch.from_numpy(feature_data_train).float()
     # import pdb;pdb.set_trace()
-    # x_f_att_z = model.nonlinear_transformation(x_f_att)
-    # x_img_att_z = model.nonlinear_transformation( x_img_att)
-    # output1  = model(image_data, adjacency_matrix ,adj_train_img)
+    image_data_flatten = torch.flatten(image_data_train, start_dim=1)
+    image_data_flatten = image_data_flatten 
+
+    # adj_train_img = kneighbors_graph(np.array(image_data_flatten), 200, mode='connectivity', include_self=True).toarray()
+
+    adj_train_img = build_knn_graph(image_data_flatten,300).float()
     # import pdb;pdb.set_trace()
-    # output = model(image_data, adjacency_matrix)
-    # model.nonlinear_transformation
-    # 计算损失
-#     y = torch.tensor(label_3_300_train).to(device)
-    y = torch.tensor(label_return(class_name, "train")).to(device)
-    # import pdb;pdb.set_trace()
-    # loss = criterion(output, feature_data)
-    # loss = criterion(output, y, 0.03)
-    # import pdb;pdb.set_trace()
-    # loss = criterion(output, y)
-    # loss1 = criterion1(output1, y)
-    # loss2 = criterion1(output2, y)
-    # loss3 = criterion2(x_f_att_z, x_img_att_z, adjacency_matrix ,adj_train_img, y)
-#     
-    loss1 = criterion5(output1, y)
-    loss2 = criterion5(output2, y)
-    
-    adj = adj_train_img +  adj_f_knn_train
-    diag = torch.diag(adj.sum(dim=1))
-    # loss3 = criterion2( emb, adj_train_img, adj_f_knn_train, y)
-    loss4 = criterion3(output1, output2, adj, diag )
-    # loss4 = criterion3(emb, adj_train_img, adj_f_knn_train, y)
+    # adj_train_img = torch.from_numpy(adj_train_img).float()
+
+
+    image_data_test = torch.from_numpy(raw_image_test ).float()
+    image_data_test = torch.tensor(image_data_test ).transpose(1,3)
+    data_features_test = raw_f_test 
+    test_feature_data = torch.from_numpy(data_features_test).float()
+
+    # 创建测试用的邻接矩阵（这里假设所有病人之间都有连接）
+    # test_adjacency_matrix = torch.ones((100, 100))
+
+
+    ## testing image adj
+
+    image_data_test_flatten = torch.flatten(image_data_test, start_dim=1)
+    # image_data_test_flatten = image_data_test
+    # adj_test_img = kneighbors_graph(np.array(image_data_test_flatten), 200, mode='connectivity', include_self=True).toarray()
+
+    adj_test_img = build_knn_graph(image_data_test_flatten, 200).float()
+
+
+    adj_f_knn_train =  build_knn_graph(raw_f_train, 300).float()
+    # adj_f_knn_train = adj_f_knn_train.toarray()
+    # adj_f_knn_train = torch.from_numpy(adj_f_knn_train).float()
+    # adj_f_knn_test = kneighbors_graph(np.array(raw_f_test), 300, mode='connectivity', include_self=True)
+    adj_f_knn_test = build_knn_graph(raw_f_test, 300).float()
+
+    projection = Projection(262, 3)
+    model = Model(projection, resnet).to(device)
     
     
 
-    
-    # loss4 = criterion4(emb, y)
+    class_weights = torch.tensor([0.5, 0.5,0.5])  # 自定義的類別權重
+    criterion1 = WeightedCrossEntropyLoss(weight=class_weights)
 
-#     loss = loss1 + loss2
+    if loss_select == 'Contrastive_loss':
+         criterion2 = contrastive_loss
 
-    alpha = 0.4
-#     loss = (1-alpha)*(loss1 + loss2) + alpha* loss3
-    # loss = (1-alpha)*(loss1 + loss2) + alpha* loss3
-    # loss = (1-alpha)*loss1  + alpha * loss2
-    
-    loss = (1-alpha)*(loss1 + loss2) + alpha* loss4
-#     loss = loss1 + loss2 + loss3 + loss4
+    elif loss_select == 'MGEC_loss':
+        criterion2 = MGECLoss()
+        
+    elif loss_select == 'InfoNCE_loss':
+        criterion2 = info_loss
+        
+    elif loss_select == 'SAC_loss':
+        criterion2 = SACLoss()
 
-    
-    # import pdb;pdb.set_trace()
-    # 反向传播和优化
-    # loss.requires_grad_(True)
-    loss.backward()
-    optimizer.step()
 
-    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, n_epochs, loss.item()))
+    # criterion3 = loss_dependence
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    class_name = classes
+    n_epochs = epoch
 
-model.eval()
-with torch.no_grad():
-    # import pdb;pdb.set_trace()
-    image_data_test = image_data_test.to(device)
-    test_feature_data = test_feature_data.to(device)
-    adj_test_img = adj_test_img.to(device)
-    adj_f_knn_test = adj_f_knn_test.to(device)
-    
-    test_output1, test_output2, emb  = model(image_data_test, test_feature_data , adj_test_img, adj_f_knn_test  )
+    training_range = tqdm(range(n_epochs))
 
-    # test_output1  = model(test_image_data, test_adjacency_matrix, adj_test_img )
-    m = nn.Softmax(dim=1)
-    # import pdb;pdb.set_trace()
-    test_output = test_output1 + test_output2
-#     test_output = emb
+    for epoch in training_range:
+        optimizer.zero_grad()
+        # cnn_z  =  cnn_encoder(image_data)
+        # 前向传播
+        
+        image_data_train = image_data_train.to(device)
+        feature_data_train = feature_data_train.to(device)
+        adj_train_img = adj_train_img.to(device)
+        adj_f_knn_train = adj_f_knn_train.to(device)
+        
+        
+        output1, output2, emb = model(image_data_train , feature_data_train,adj_train_img, adj_f_knn_train)
 
-    pred =  m(test_output).argmax(dim=1)
-    # test_output = test_output.argmax(dim=1)
-    
-    
-    # y_test = torch.empty(100).random_(2)
-#     y_test = torch.tensor(label_3_test).to(device)
-    y_test = torch.from_numpy(label_return(class_name, "test")).to(device)
+        y = torch.tensor(label_return(class_name, "train")).to(device)
 
+        loss_ce1 = criterion1(output1, y)
+        loss_ce2 = criterion1(output2, y)
+
+        if loss_select == 'Contrastive_loss':
+            loss_extra = criterion2( emb, adj_train_img, adj_f_knn_train, y)
+
+        elif loss_select == 'MGEC_loss':
+            adj = adj_train_img +  adj_f_knn_train
+            diag = torch.diag(adj.sum(dim=1))
+            loss_extra = criterion2(output1, output2, adj, diag )
+
+        elif loss_select == 'InfoNCE_loss':
+            loss_extra = criterion2( emb, adj_train_img, adj_f_knn_train, y)
+
+        elif loss_select == 'SAC_loss':
+                adj = adj_train_img +  adj_f_knn_train
+                diag = torch.diag(adj.sum(dim=1))
+                loss_extra = criterion2(output1, output2, adj)
+
+        alpha = 0.4
+       
+        loss = (1-alpha)*(loss_ce1 + loss_ce2) + alpha* loss_extra
+
+        loss.backward()
+        optimizer.step()
+
+        # print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, n_epochs, loss.item()))
+
+    model.eval()
+    with torch.no_grad():
+        # import pdb;pdb.set_trace()
+        image_data_test = image_data_test.to(device)
+        test_feature_data = test_feature_data.to(device)
+        adj_test_img = adj_test_img.to(device)
+        adj_f_knn_test = adj_f_knn_test.to(device)
+        
+        test_output1, test_output2, emb  = model(image_data_test, test_feature_data , adj_test_img, adj_f_knn_test  )
+
+        # test_output1  = model(test_image_data, test_adjacency_matrix, adj_test_img )
+        m = nn.Softmax(dim=1)
+        # import pdb;pdb.set_trace()
+        test_output = test_output1 + test_output2
+    #     test_output = emb
+
+        pred =  m(test_output).argmax(dim=1)
+        # test_output = test_output.argmax(dim=1)
+        
+        
+        # y_test = torch.empty(100).random_(2)
+    #     y_test = torch.tensor(label_3_test).to(device)
+        y_test = torch.from_numpy(label_return(class_name, "test")).to(device)
+
+        
+        correct = (pred  == y_test).sum().item()
+        accuracy = correct / len(y_test)
+        print("class_name",class_name,"Accuracy:", accuracy)
+        # import pdb;pdb.set_trace()
+        print(calculate_metrics_new(y_test.cpu().detach().numpy(), pred.cpu().detach().numpy() ))
+        print(classification_report(y_test.cpu().detach().numpy(), pred.cpu().detach().numpy() ))
+        
+        plot_ROC(pred.cpu().detach().numpy() , y_test.cpu().detach().numpy(), 3)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img_data_dir', type=str, default='/Users/test/Documents/Contrastive_PD/skin_dataset_ok/')
+    parser.add_argument('--skin_type', type=str)
+    parser.add_argument('--meta_data_dir', type=str, default='/Users/test/Documents/Contrastive_PD/skin_dataset_ok/meta_ok/')
+    parser.add_argument('--losses_choice', type=str)
+    parser.add_argument('--classes', type=str)
+    parser.add_argument('--n_epoch', type=int)
     
-    correct = (pred  == y_test).sum().item()
-    accuracy = correct / len(y_test)
-    print("class_name",class_name,"Accuracy:", accuracy)
-    # import pdb;pdb.set_trace()
-    print(calculate_metrics_new(y_test.cpu().detach().numpy(), pred.cpu().detach().numpy() ))
-    print(classification_report(y_test.cpu().detach().numpy(), pred.cpu().detach().numpy() ))
-    
-    plot_ROC(pred.cpu().detach().numpy() , y_test.cpu().detach().numpy(), 3)
+    args = parser.parse_args()
+    train_eval(args.img_data_dir, args.skin_type, args.meta_data_dir, args.losses_choice, args.classes, args.n_epoch)
+
+
+if __name__ == '__main__':
+    main()
